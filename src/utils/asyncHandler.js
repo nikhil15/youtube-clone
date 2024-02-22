934f664cd8e02e6954ae2aa2475adc56d5afd8b0
Nikhil <nikhilgudadhe63@gmail.com>
2024-02-22 11:16:08 +0530

    added register controller and bug fixes

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -4,15 +4,4 @@ const asyncHandler = (requestHandler) => {
     }
 }
 
-// const asyncHandler = (func) => async (req, res, next) => {
-//     try {
-//         await func(req, res, next)       
-//     } catch (error) {
-//        res.status(error.code || 500).json({
-//             success: false,
-//             message: error.message
-//        }) 
-//     }
-// }
-
 export { asyncHandler }
