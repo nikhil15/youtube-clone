e29799bf3a372cc7119bf2f38b3dec3c992e3631
Nikhil <nikhilgudadhe63@gmail.com>
2024-02-24 07:58:44 +0530

    added user login and logout feature

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,26 +4,31 @@ import { User } from "../models/user.model.js"
 import { uploadOnCloudinary } from "../utils/cloudinary.js"
 import { apiResponse } from "../utils/apiResponse.js"
 
-const registerUser = asyncHandler( async (req, res) => {
-    // get user details from frontend
-    // validation - not empty
-    // check if user already exists (username,email)
-    // check for images, check for avatar
-    // upload them to cloudinary, avatar 
-    // create user object - create entry in db
-    // remove password and refresh token field
-    // check for user creation
-    // return response
+const generateAccessAndRefreshTokens = async (userId) => {
+    try {
+        const user = await User.findById({userId}) 
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+        
+        user.refreshToken = refreshToken
+        await user.save({validateBeforeSave: false})
 
-    //const {username, email, fullName, avatar, coverImage, password } = req.body 
-    const {username, email, password } = req.body 
-    console.log(username, email, password)
+        return { accessToken, refreshToken }
+
+    } catch (error) {
+        throw new apiError(500, "Something went wrong while generating refresh and access token")
+    }
+}
+
+const registerUser = asyncHandler( async (req, res) => {
+
+    const {username, email, fullName, password } = req.body 
 
     if([fullName, email, username, password ].some((field) => { field?.trim() === ""})) {
         throw new apiError(400, "All fields are required")
     }
 
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{ username }, { email }]
     })
 
@@ -32,8 +37,11 @@ const registerUser = asyncHandler( async (req, res) => {
     }
 
     const avatarLocalPath = req.files?.avatar[0]?.path
-    const coverImageLocalPath = req.field?.coverImage[0]?.path
-    console.log(req.files)
+    const coverImageLocalPath = res?.files?.coverImage?.[0]?.path
+    
+    // if(req.files && Array.isArray(req.files.coverImage) && req.files.length > 0) {
+    //     coverImageLocalPath = req.files?.coverImage[0]?.path
+    // }
 
     if(!avatarLocalPath) {
         throw new apiError(409, "Avtar file is required")
@@ -69,4 +77,64 @@ const registerUser = asyncHandler( async (req, res) => {
 
 })
 
-export { registerUser }
+const loginUser = asyncHandler( async(req, res) => {
+
+    const {username, email, password } = req.body 
+
+    if( !(username || email) ) {
+        throw new apiError(400, "Username or email is required")
+    }
+
+    const user = await User.findOne({
+        $or: [{ username }, { email }]
+    })
+
+    if(!user) {
+        throw new apiError(404, "User does not exists")
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password)
+
+    if(!isPasswordValid) {
+        throw new apiError(401, "Invalid user credentials")
+    }
+
+    const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(user._id)
+
+    const loggedInUser = User.findById(user._id).select("-password -refreshToken")
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res.status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+        new apiResponse(200, {
+            user: loggedInUser, accessToken, refreshToken
+        }),
+        "User logged in successfully"
+    )
+
+})
+
+const logoutUser = asyncHandler(async(req, res) => {
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: { refreshToken: undefined }
+        }, 
+        {
+            new: true
+        }
+    )
+
+    return res.status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new apiResponse(200, {}, "User logged out"))
+})
+
+export { registerUser, loginUser, logoutUser }
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -15,7 +15,7 @@ const uploadOnCloudinary = async (localFilePath) => {
             resource_type: "auto"
         })
         // file has been uploaded successfully
-        console.log("file is uploaded on cloudinary", response.url)
+        fs.unlinkSync(localFilePath)
         return response
     } catch (error) {
         // remove the locally saved temporary files as the upload operation got failed. 
